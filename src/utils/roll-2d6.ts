5c89e6a0a2987f6edef2f7881296fc8856fa2187
Alfonso, Montes de Oca <montesdeoca.alfonso@gmail.com>
2024-07-04 14:22:22 -0300

    feat: util for roll and description

--- src/utils/roll-2d6.ts
+++ src/utils/roll-2d6.ts
@@ -1,5 +1,22 @@
+import type { Characteristic } from 'types/character';
+
 export function roll2d6(mod: number) {
   const r1d6 = () => Math.floor(Math.random() * 6);
   const result = r1d6() + r1d6();
   return result + mod;
+}
+
+export function esMsgRoll2d6(name: string, char: Characteristic, mod: number): string {
+  const charNames = {
+    strength: 'Fuerza',
+    dexterity: 'Destreza',
+    constitution: 'Constitución',
+    intelligence: 'Inteligencia',
+    wisdom: 'Sabiduría',
+    charisma: 'Carisma',
+  } as { [key in Characteristic]: string };
+
+  return `${name}, realiza una prueba de 2d6 +${mod} (${charNames[char]}) y obtiene: ${roll2d6(
+    mod
+  )}.`;
 }
