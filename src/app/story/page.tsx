65375ea705879d6c8c283a58445c713cebf897ee
Alfonso, Montes de Oca <montesdeoca.alfonso@gmail.com>
2024-07-04 14:32:42 -0300

    refactor

--- src/app/story/page.tsx
+++ src/app/story/page.tsx
@@ -2,7 +2,7 @@
 
 import CardPlayCharacter from 'components/card-play-character';
 import { useCharacterStore } from 'hooks/use-character-store';
-import { Character, Characteristic } from 'types/character';
+import { Characteristic } from 'types/character';
 import { esMsgRoll2d6 } from 'utils/roll-2d6';
 
 export default function Page() {
@@ -21,21 +21,21 @@ export default function Page() {
     <main className="flex min-h-screen flex-col items-center justify-between">
       <h1>Historia</h1>
 
+      <section className="flex justify-center items-center gap-4">
+        <h3 className="font-bold">Opciones: </h3>
+        <button className="btn btn-circle" onClick={() => onHistoryOptionClick('A')}>
+          A
+        </button>
+        <button className="btn btn-circle" onClick={() => onHistoryOptionClick('B')}>
+          B
+        </button>
+        <button className="btn btn-circle" onClick={() => onHistoryOptionClick('C')}>
+          C
+        </button>
+      </section>
+
       <section className="my-4">
         <h2 className="text-center text-2xl font-bold mb-4">Personajes Jugadores</h2>
-
-        <div className="flex justify-center items-center gap-4">
-          <h3 className="font-bold">Opciones de historia: </h3>
-          <button className="btn btn-circle" onClick={() => onHistoryOptionClick('A')}>
-            A
-          </button>
-          <button className="btn btn-circle" onClick={() => onHistoryOptionClick('B')}>
-            B
-          </button>
-          <button className="btn btn-circle" onClick={() => onHistoryOptionClick('C')}>
-            C
-          </button>
-        </div>
 
         <div className="flex flex-wrap gap-4 justify-center">
           {inGameCharacters.map((character) => (
