12e3611aadd482cdb1d56e85ce0a78ce4ffa6c28
Alfonso, Montes de Oca <montesdeoca.alfonso@gmail.com>
2024-07-04 10:33:55 -0300

    fix: title

--- src/app/new-character/page.tsx
+++ src/app/new-character/page.tsx
@@ -3,7 +3,7 @@ import FormCharacterDescription from 'components/form-character-description';
 export default function Page() {
   return (
     <main className="container flex flex-col items-center justify-center">
-      <h1 className="text-3xl font-bold mb-4">Crear Tu Personaje</h1>
+      <h1 className="text-3xl font-bold mb-4">Creación de Personaje</h1>
 
       <FormCharacterDescription />
     </main>
