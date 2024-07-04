d89ab395030dbcf3b7e32b6f93adaec53eec2cba
Alfonso, Montes de Oca <montesdeoca.alfonso@gmail.com>
2024-07-04 10:35:18 -0300

    feat: clear state option

--- src/hooks/use-create-new-character-state.tsx
+++ src/hooks/use-create-new-character-state.tsx
@@ -3,8 +3,29 @@ import { Character } from 'types/character';
 
 type CharacterKey = keyof Character;
 
+const initialCharacterState: Character = {
+  id: crypto.randomUUID(),
+  xp: 60,
+  name: '',
+  appareance: '',
+  background: '',
+  equipment: '',
+  personality: '',
+  profession: '',
+  powers: '',
+  characteristics: {
+    strength: 3,
+    dexterity: 3,
+    constitution: 3,
+    intelligence: 3,
+    wisdom: 3,
+    charisma: 3,
+  },
+};
+
 interface CreateNewCharacterStoreActions {
   setAllCharacterInfo: (character: Character) => void;
+  clearAllCharacterInfo: () => void;
   setDescription: ({ key, value }: { key: CharacterKey; value: string }) => void;
   setCharacteristic: ({
     key,
@@ -18,26 +39,12 @@ interface CreateNewCharacterStoreActions {
 
 export const useCreateNewCharacterStore = create<Character & CreateNewCharacterStoreActions>(
   (set) => ({
-    id: crypto.randomUUID(),
-    xp: 60,
-    name: '',
-    appareance: '',
-    background: '',
-    equipment: '',
-    personality: '',
-    profession: '',
-    powers: '',
-    characteristics: {
-      strength: 3,
-      dexterity: 3,
-      constitution: 3,
-      intelligence: 3,
-      wisdom: 3,
-      charisma: 3,
-    },
+    ...initialCharacterState,
 
     // Actions
     setAllCharacterInfo: (character) => set(character),
+
+    clearAllCharacterInfo: () => set(initialCharacterState),
 
     setDescription: ({ key, value }) => set((state) => ({ ...state, [key]: value })),
 
