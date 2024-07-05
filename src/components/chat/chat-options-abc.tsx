c3e025af65bc85aec60cc4bd49963909a9f15e05
Alfonso, Montes de Oca <montesdeoca.alfonso@gmail.com>
2024-07-05 01:07:47 -0300

    feat: finish story

--- src/components/chat/chat-options-abc.tsx
+++ src/components/chat/chat-options-abc.tsx
@@ -1,9 +1,15 @@
 'use client';
 
+import { useCharacterStore } from 'hooks/use-character-store';
 import { useGmAiStore } from 'hooks/use-gm-ai-chat-store';
+import { useLibraryStore } from 'hooks/use-library-store';
+import { useModalState } from 'hooks/use-modal-state';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function ChatOptionsABC() {
   const { addContent, isLoadingContent } = useGmAiStore();
+  const { setModalContent, setModalIsOpen } = useModalState();
 
   function onHistoryOptionClick(option: 'A' | 'B' | 'C'): void {
     addContent({
@@ -12,30 +18,98 @@ export default function ChatOptionsABC() {
     });
   }
 
+  function onEndHistoryClick(): void {
+    setModalContent(<ModalEndHistory />);
+    setModalIsOpen(true);
+  }
+
   return (
-    <section className="flex justify-center items-center gap-4">
+    <section className="flex flex-wrap justify-center items-center gap-4">
       <h3 className="font-bold">Opciones: </h3>
-      <button
-        className="btn btn-circle"
-        onClick={() => onHistoryOptionClick('A')}
-        disabled={isLoadingContent}
-      >
-        A
-      </button>
-      <button
-        className="btn btn-circle"
-        onClick={() => onHistoryOptionClick('B')}
-        disabled={isLoadingContent}
-      >
-        B
-      </button>
-      <button
-        className="btn btn-circle"
-        onClick={() => onHistoryOptionClick('C')}
-        disabled={isLoadingContent}
-      >
-        C
+
+      <div className="flex gap-2">
+        <button
+          className="btn btn-circle"
+          onClick={() => onHistoryOptionClick('A')}
+          disabled={isLoadingContent}
+        >
+          A
+        </button>
+        <button
+          className="btn btn-circle"
+          onClick={() => onHistoryOptionClick('B')}
+          disabled={isLoadingContent}
+        >
+          B
+        </button>
+        <button
+          className="btn btn-circle"
+          onClick={() => onHistoryOptionClick('C')}
+          disabled={isLoadingContent}
+        >
+          C
+        </button>
+      </div>
+
+      <button className="btn btn-error" onClick={onEndHistoryClick}>
+        Finalizar Historia
       </button>
     </section>
+  );
+}
+
+function ModalEndHistory() {
+  const router = useRouter();
+  const { setModalIsOpen } = useModalState();
+  const { storyName, storyId, setStoryName, setHistoryId, content, resetChat } = useGmAiStore();
+  const { inGameCharacters } = useCharacterStore();
+
+  const { addBook } = useLibraryStore();
+
+  useEffect(() => {
+    setHistoryId();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  function onSaveBookClick() {
+    if (storyName)
+      addBook({
+        id: storyId,
+        title: storyName,
+        characters: inGameCharacters,
+        content,
+      });
+
+    resetChat(); // reset to inital state
+    setModalIsOpen(false);
+    router.push('/');
+  }
+
+  return (
+    <div>
+      <h3 className="font-bold text-lg">Fin de la Historia</h3>
+      <p className="py-4">
+        Una vez finalices la historia, no podras volver a ella para seguir jugándola. <br />
+        Si colocas un nombre a tu historia, podrás guardarla para leerla en tu biblioteca. <br />
+        <strong>Elige un nombre original.</strong>
+      </p>
+
+      <input
+        className="input input-bordered w-full text-center"
+        type="text"
+        placeholder="Nombre de la Historia"
+        value={storyName}
+        onChange={(e) => setStoryName(e.target.value)}
+      />
+
+      <div className="modal-action justify-around">
+        <button className="btn btn-error" onClick={onSaveBookClick}>
+          Si, finalizar historia
+        </button>
+        <button className="btn btn-success" onClick={() => setModalIsOpen(false)}>
+          No, seguir jugando
+        </button>
+      </div>
+    </div>
   );
 }
