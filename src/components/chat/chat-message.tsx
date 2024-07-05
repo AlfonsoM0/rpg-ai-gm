c5dde04acc9f6321113b452dc18869446df2395b
Alfonso, Montes de Oca <montesdeoca.alfonso@gmail.com>
2024-07-05 14:49:45 -0300

    feat: final messege can guive XP

--- src/components/chat/chat-message.tsx
+++ src/components/chat/chat-message.tsx
@@ -1,4 +1,9 @@
+'use client';
+
+import { useCharacterStore } from 'hooks/use-character-store';
+import { useModalState } from 'hooks/use-modal-state';
 import Markdown from 'markdown-to-jsx';
+import Image from 'next/image';
 
 interface ChatMsgStart {
   userName: string;
@@ -9,7 +14,21 @@ interface ChatMsgStart {
 }
 
 export default function ChatMessage({ userName, message, position }: ChatMsgStart) {
-  const chatPosition = position === 'start' ? 'chat chat-start' : 'chat chat-end';
+  const { inGameCharacters, findCharacterByIdAndIcrementXp } = useCharacterStore();
+  const { setModalContent, setModalIsOpen } = useModalState();
+
+  const win1XP = message.includes('⬆️UP+1XP') ? 1 : 0;
+  const win2XP = message.includes('⬆️UP+2XP') ? 2 : 0;
+
+  if (win1XP || win2XP) {
+    const winXp = win1XP + win2XP;
+    inGameCharacters.forEach((character) => {
+      findCharacterByIdAndIcrementXp(character.id, winXp);
+    });
+
+    setModalContent(ModalWinXp);
+    setModalIsOpen(true);
+  }
 
   if (message.includes('**Player Characters**'))
     return (
@@ -18,13 +37,17 @@ export default function ChatMessage({ userName, message, position }: ChatMsgStar
       </div>
     );
 
+  const chatPosition = position === 'start' ? 'chat chat-start' : 'chat chat-end';
+
   return (
     <div className={chatPosition}>
       <div className="chat-image avatar">
         <div className="w-10 rounded-full">
-          <img
-            alt="Tailwind CSS chat bubble component"
+          <Image
+            alt={userName}
             src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+            width={10}
+            height={10}
           />
         </div>
       </div>
@@ -52,3 +75,10 @@ const mdOpt = {
     },
   },
 };
+
+const ModalWinXp = (
+  <div>
+    <h3 className="font-bold text-lg">Has ganado Puntos de Experiencia (XP)</h3>
+    <p className="py-4">Regresa a la pagina principal para editar tu personaje.</p>
+  </div>
+);
