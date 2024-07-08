c2c104b8212857511445ea90f05752dd52ec8930
Alfonso, Montes de Oca <montesdeoca.alfonso@gmail.com>
2024-07-08 14:26:27 -0300

    fix svg

--- src/components/icons/icon-stars.tsx
+++ src/components/icons/icon-stars.tsx
@@ -15,7 +15,7 @@ export function Stars(props: ComponentProps<'svg'>): JSX.Element {
       {...props}
     >
       <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-      <g id="SVGRepo_tracerCarrier" stroke-linecap="round" strokeLinejoin="round"></g>
+      <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
       <g id="SVGRepo_iconCarrier">
         {' '}
         <g>
