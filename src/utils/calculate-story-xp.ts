adf049fbfbb2b05e8d3cd6727a5fbf049462c2cf
Alfonso, Montes de Oca <montesdeoca.alfonso@gmail.com>
2024-07-11 12:06:09 -0300

    refactor fix

--- src/utils/calculate-story-xp.ts
+++ src/utils/calculate-story-xp.ts
@@ -9,13 +9,20 @@ export function calculateStoryXp(diceRolls: number[]) {
   let roll10orMore = [];
   let roll14orMore = [];
 
-  for (let i = 0; i < diceRolls.length; i++) {
-    const roll = diceRolls[i];
-    const totalSuccesses = roll10orMore.length + roll14orMore.length * 2;
-    const totalFailures = roll7orMore.length + roll6orLess.length * 2;
+  let totalSuccesses = 0;
+  let totalFailures = 0;
 
-    if (totalFailures >= 3 || totalSuccesses >= 5) {
-      // Story is over
+  for (let i = 0; i <= diceRolls.length; i++) {
+    totalFailures = roll7orMore.length + roll6orLess.length * 2;
+    totalSuccesses = roll10orMore.length + roll14orMore.length * 2;
+
+    if (totalFailures < 3 || totalSuccesses < 5) {
+      const roll = diceRolls[i];
+      if (roll <= 6) roll6orLess.push(roll);
+      if (roll >= 7 && roll < 10) roll7orMore.push(roll);
+      if (roll >= 10 && roll < 14) roll10orMore.push(roll);
+      if (roll >= 14) roll14orMore.push(roll);
+    } else {
       let storyXp = 2;
       if (totalSuccesses >= 4) storyXp += 2;
 
@@ -25,19 +32,12 @@ export function calculateStoryXp(diceRolls: number[]) {
         isStoryOver: true,
         storyXp,
       };
-    } else {
-      // Story is not over yet
-      if (roll <= 6) roll6orLess.push(roll);
-      if (roll >= 7 && roll < 10) roll7orMore.push(roll);
-      if (roll >= 10 && roll < 14) roll10orMore.push(roll);
-      if (roll >= 14) roll14orMore.push(roll);
     }
   }
 
-  // If the story is not over, return the current state of the story
   return {
-    totalSuccesses: roll10orMore.length + roll14orMore.length * 2,
-    totalFailures: roll7orMore.length + roll6orLess.length * 2,
+    totalSuccesses,
+    totalFailures,
     isStoryOver: false,
     storyXp: 0,
   };
