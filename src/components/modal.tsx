def2a8ef8168d371dc98eae2f0b4fb4c882a04fb
Alfonso, Montes de Oca <montesdeoca.alfonso@gmail.com>
2024-07-13 17:37:47 -0300

    feat: modal close on outside click

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,10 +1,12 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useModalState } from 'hooks/use-modal-state';
 
 export function Modal(): JSX.Element {
   const { isOpen, content, setModalIsOpen, setModalContent } = useModalState();
+
+  const refDialog = useRef<HTMLDivElement>(null);
 
   // clean up on close
   useEffect(() => {
@@ -12,9 +14,13 @@ export function Modal(): JSX.Element {
     // eslint-disable-next-line react-hooks/exhaustive-deps -- avoid circular dependency
   }, [isOpen]);
 
+  function handleClickOutside(e: React.MouseEvent<HTMLDialogElement, MouseEvent>) {
+    if (refDialog.current && !refDialog.current.contains(e.target as Node)) setModalIsOpen(false);
+  }
+
   return (
-    <dialog className="modal" open={isOpen}>
-      <div className="modal-box">
+    <dialog className="modal backdrop-blur-sm" open={isOpen} onClick={handleClickOutside}>
+      <div className="modal-box border-2" ref={refDialog}>
         <button
           className="btn btn-sm btn-circle absolute right-3 top-3"
           onClick={() => setModalIsOpen(false)}
