a2b6518ecd664ac5ecd3bb74379bcb3501291ca5
Alfonso, Montes de Oca <montesdeoca.alfonso@gmail.com>
2024-07-24 12:45:19 -0300

    fix: modal

--- src/components/chat/chat-options-modals/modal-ai-ideas/index.tsx
+++ src/components/chat/chat-options-modals/modal-ai-ideas/index.tsx
@@ -1,20 +1,22 @@
 import CollapseTips from './collapse-tips';
 import CollapseMyShortcuts from './collapse-my-shortcuts';
 import CollapseShortcuts from './collapse-shortcuts';
+import { ModalContentContainer } from 'components/modal';
 
 export default function ModaIdeasForAI() {
   return (
-    <div>
-      <h3 className="font-bold text-lg">Tips y Atajos</h3>
-      <p className="py-4 text-sm">
-        Estos son tips y atajos para ayudarte a mejorar tu experiencia con Game Master AI. <br />
-      </p>
+    <ModalContentContainer title="Tips y Atajos" titleColor="info">
+      <>
+        <p className="py-4 text-sm">
+          Estos son tips y atajos para ayudarte a mejorar tu experiencia con Game Master AI. <br />
+        </p>
 
-      <CollapseTips />
+        <CollapseTips />
 
-      <CollapseMyShortcuts />
+        <CollapseMyShortcuts />
 
-      <CollapseShortcuts />
-    </div>
+        <CollapseShortcuts />
+      </>
+    </ModalContentContainer>
   );
 }
