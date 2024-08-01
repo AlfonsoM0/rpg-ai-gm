a51344229d6cc11f6897f1183b617d51031cd02c
Alfonso, Montes de Oca <montesdeoca.alfonso@gmail.com>
2024-08-01 09:17:50 -0300

    feat: voice i18n

--- src/components/tts/tts-config.tsx
+++ src/components/tts/tts-config.tsx
@@ -2,8 +2,11 @@
 
 import { useTTSStore } from 'hooks/use-tts-store';
 import TTSControls from './tts-controls';
+import { useTranslations } from 'next-intl';
 
 export default function TTSConfig() {
+  const t = useTranslations('TTS.TTSConfig');
+
   const {
     isTTSEnabled,
     voices,
@@ -19,11 +22,20 @@ export default function TTSConfig() {
     handleStop,
   } = useTTSStore();
 
+  const voiceConfigTips = (
+    <div className="m-2">
+      <ul className="list-disc text-info">
+        <li>{t('tips.1')}</li>
+        <li>{t('tips.2')}</li>
+      </ul>
+    </div>
+  );
+
   if (!speechSynthesis || !voices.length)
     return (
       <>
-        <h4 className="text-center my-4 font-bold">Voz</h4>
-        <p className="text-center">No disponible en tu navegador.</p>
+        <h4 className="text-center my-4 font-bold">{t('Voice')}</h4>
+        <p className="text-center">{t('Voice_not_available')}</p>
         {voiceConfigTips}
       </>
     );
@@ -32,7 +44,7 @@ export default function TTSConfig() {
     <>
       <div className="form-control w-24 m-auto mt-8">
         <label className="label cursor-pointer">
-          <span className="label-text font-bold text-lg text-info">Voz</span>
+          <span className="label-text font-bold text-lg text-info">{t('Voice')}</span>
           <input
             type="checkbox"
             className="toggle toggle-info"
@@ -51,7 +63,7 @@ export default function TTSConfig() {
 
           <div className="flex items-center gap-2">
             <div className="w-24">
-              <p>Locutor: </p>
+              <p>{t('Broadcaster')}: </p>
             </div>
             <select
               className="select select-bordered w-full"
@@ -70,7 +82,7 @@ export default function TTSConfig() {
 
           <div className="flex justify-between items-center gap-2">
             <div className="w-24">
-              <p>Tono: </p>
+              <p>{t('Pitch')}: </p>
             </div>
             <input
               className="range"
@@ -85,7 +97,7 @@ export default function TTSConfig() {
 
           <div className="flex items-center gap-2">
             <div className="w-24">
-              <p>Velocidad: </p>
+              <p>{t('Rate')}: </p>
             </div>
             <input
               className="range"
@@ -100,7 +112,7 @@ export default function TTSConfig() {
 
           <div className="flex items-center gap-2">
             <div className="w-24">
-              <p>Volumen: </p>
+              <p>{t('Volume')}: </p>
             </div>
             <input
               className="range"
@@ -119,16 +131,3 @@ export default function TTSConfig() {
     </>
   );
 }
-
-const voiceConfigTips = (
-  <div className="m-2">
-    <ul className="list-disc text-info">
-      <li>Haz clic en los avatares del chat para reproducir el texto.</li>
-      <li>
-        Es posible que tengas que configurar tu dispositivo y descargar paquetes de voz para
-        escuchar el texto. Busca &quot;salida de texto a voz&quot; (TTS) en la configuración de
-        idioma.
-      </li>
-    </ul>
-  </div>
-);
