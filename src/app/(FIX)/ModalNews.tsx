b9345f198712fac4b4f6ba24fdea1a07c9d569d0
Alfonso, Montes de Oca <montesdeoca.alfonso@gmail.com>
2024-08-01 10:15:49 -0300

    refactor

--- src/app/(FIX)/ModalNews.tsx
+++ src/app/(FIX)/ModalNews.tsx
@@ -8,33 +8,31 @@ export default function ModalNews() {
 
   return (
     <ModalContentContainer title={'¡Advertencia!'} titleColor="warning">
-      <>
-        <p className="my-4">
-          Este juego no está completo y puedes perder información en alguna actualización. <br />
-          <br />
-          ¡Ya puedes gardar tus datos iniciando sessión con una <strong>
-            cuenta de Google
-          </strong>! <br />
-          <br />
-          Para notificar un error o dar una sugerencia puedes escribirme en{' '}
-          <a
-            className="link link-info"
-            href="https://alfonso.ar/contact"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            alfonso.ar/contact
-          </a>{' '}
-          (o haciendo click en el pie de página).
-        </p>
-
-        <button
-          onClick={() => setModalIsOpen(false)}
-          className="btn btn-primary mt-4 w-full text-xl font-bold"
+      <p className="my-4">
+        Este juego no está completo y puedes perder información en alguna actualización. <br />
+        <br />
+        ¡Ya puedes gardar tus datos iniciando sessión con una <strong>
+          cuenta de Google
+        </strong>! <br />
+        <br />
+        Para notificar un error o dar una sugerencia puedes escribirme en{' '}
+        <a
+          className="link link-info"
+          href="https://alfonso.ar/contact"
+          rel="noopener noreferrer"
+          target="_blank"
         >
-          ¡Entendido!
-        </button>
-      </>
+          alfonso.ar/contact
+        </a>{' '}
+        (o haciendo click en el pie de página).
+      </p>
+
+      <button
+        onClick={() => setModalIsOpen(false)}
+        className="btn btn-primary mt-4 w-full text-xl font-bold"
+      >
+        ¡Entendido!
+      </button>
     </ModalContentContainer>
   );
 }
