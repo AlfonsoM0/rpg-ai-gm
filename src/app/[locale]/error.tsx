aaecd830e94c4ea7a55d099ad0e204a3596f5786
Alfonso, Montes de Oca <montesdeoca.alfonso@gmail.com>
2024-08-06 16:16:23 -0300

    feat: error handler

---
+++ src/app/[locale]/error.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { useEffect } from 'react';
+import H2 from 'src/components/h2';
+import Main from 'src/components/Main';
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return (
+    <Main>
+      <H2>Something went wrong!</H2>
+
+      <p>
+        <strong>ERROR: </strong>
+        <br />
+        <br />
+        {error.name}: {error.message}
+      </p>
+
+      <button
+        className="btn"
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </button>
+    </Main>
+  );
+}
