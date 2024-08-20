0c6c0ef47c3b3d5be00f67ec5bed154a6f449ce5
Alfonso, Montes de Oca <montesdeoca.alfonso@gmail.com>
2024-08-20 13:42:46 -0300

    refactor: end game more info

--- src/components/chat/chat-options-modals/modal-end-multiplayer.tsx
+++ src/components/chat/chat-options-modals/modal-end-multiplayer.tsx
@@ -2,8 +2,9 @@
 
 import { useTranslations } from 'next-intl';
 import { ModalContentContainer } from 'src/components/modal';
-import { usePlayerAcctions } from 'src/hooks/multiplayer';
+import useMultiplayer, { usePlayerAcctions } from 'src/hooks/multiplayer';
 import { useModalState } from 'src/hooks/use-modal-state';
+import { calculateStoryXpMp } from 'src/utils/gmai-utils-mp';
 
 export default function ModalEndMultiplayer() {
   const t = useTranslations('ModalEndHistory_Multiplayer');
@@ -12,6 +13,14 @@ export default function ModalEndMultiplayer() {
 
   const { endGame } = usePlayerAcctions();
 
+  const { multiplayerStory } = useMultiplayer();
+
+  const xp = calculateStoryXpMp({
+    totalDiceRolls: multiplayerStory?.totalDiceRolls || 0,
+    totalSuccesses: multiplayerStory?.totalSuccesses || 0,
+    totalFailures: multiplayerStory?.totalFailures || 0,
+  });
+
   function onEndGameClick() {
     endGame();
     setModalIsOpen(false);
@@ -19,9 +28,17 @@ export default function ModalEndMultiplayer() {
 
   return (
     <ModalContentContainer title={t('title')} titleColor="info">
-      <p className="py-4">{t('p1')}</p>
+      <p className="py-4 text-error text-center">{t('p1')}</p>
       <p className="pb-4">{t('p2')}</p>
       <p className="pb-4">{t('p3')}</p>
+
+      {xp ? (
+        <p className="pb-4 text-center text-success">
+          {t('info_xp_msg')} {xp} XP
+        </p>
+      ) : (
+        <p className="pb-4 text-center text-error">{t('no_xp_msg')}</p>
+      )}
 
       <div className="modal-action justify-around">
         <button className="btn btn-error" onClick={onEndGameClick} aria-label={t('btn_End_Game')}>
